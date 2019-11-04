9d31992702d3dc52a9ccda19677678a9ad8308fa
wgr523 <wgr523@gmail.com>
2019-11-04 14:37:54 -0600

    update url, change binary name

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -172,7 +172,7 @@ fn build_rocksdb() {
     config.file("build_version.cc");
 
     config.cpp(true);
-    config.compile("librocksdb.a");
+    config.compile("librustrocksdb.a");
 }
 
 fn build_snappy() {
